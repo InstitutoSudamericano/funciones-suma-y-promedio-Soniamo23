ceac778647aeeb671fd8e2856565056f44b65bf6
jeremy <116098948+MiguelCriollo@users.noreply.github.com>
2023-07-14 10:51:51 -0500

    tarea 14

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function sumarVariable(a, b){
+    let resultado= a+b;
+    return resultado;
+}
+let variable1= 8;
+let variable2= 3;
+let resultadoSuma= sumarVariable(variable1,variable2);
+alert(resultadoSuma);
